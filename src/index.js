d76852578eb0c79a40d80820a5ea9c8d515e9338
drestew <drestew48@gmail.com>
2022-07-29 15:07:40 -0400

    Add new projects to DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css'
 import { Project } from "./project";
-import { storeProject } from './storage';
+import { getProject, getProjects, storeProject } from './storage';
 
 const projectBtn = document.querySelector('.projects-add')
 const projectForm = document.querySelector('.project-form')
@@ -10,11 +10,41 @@ const toggleForm = function () {
     projectForm.classList.toggle('hidden')
 }
 projectBtn.addEventListener('click', toggleForm)
+projectForm.addEventListener('submit', function () {
+    toggleForm()
+    this.reset()
+})
 
 saveNewItem.addEventListener('click', function () {
     const name = document.querySelector('#name').value
     const description = document.querySelector('#description').value
     const newProject = new Project(name, description)
     storeProject(newProject)
-    toggleForm()
+    addToDOM(newProject)
 })
+
+const addToDOM = function (item) {
+    const projectList = document.querySelector('.projects-list')
+    const itemEl = document.createElement('li')
+    const itemName = document.createElement('h3')
+
+    itemName.textContent = item.name
+
+    projectList.appendChild(itemEl)
+    itemEl.appendChild(itemName)
+
+    return item
+}
+
+const loadProject = function (projects) {
+    projects.projectArr.forEach(project => {
+        addToDOM(project)
+    })
+}
+
+window.addEventListener('load', function () {
+    const projects = getProjects('projects')
+    loadProject(projects)
+})
+
+
--- src/storage.js
+++ src/storage.js
@@ -1,9 +1,15 @@
-const getProject = function (projectName) {
-    return localStorage.getItem(projectName)
+const getProjects = function (projectArr) {
+    return JSON.parse(localStorage.getItem(projectArr))
 }
 
 const storeProject = function (project) {
-    return localStorage.setItem(project.name, JSON.stringify(project))
+    if (!localStorage.getItem('projects')) {
+        return localStorage.setItem('projects', JSON.stringify({ projectArr: [] }))
+    } else {
+        const curProjects = JSON.parse(localStorage.getItem('projects'))
+        curProjects.projectArr.push(project)
+        return localStorage.setItem('projects', JSON.stringify(curProjects))
+    }
 }
 
-export { storeProject, getProject }
+export { storeProject, getProjects }
