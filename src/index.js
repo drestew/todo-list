44e09be4bb0753d2629a7aa09b40c78940e360ec
drestew <drestew48@gmail.com>
2022-07-28 20:51:47 -0400

    Add basic functionality for creating a project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('working')
+import './style.css'
+import { Project } from "./project";
+
+const projectBtn = document.querySelector('.projects-add')
+projectBtn.addEventListener('click', function () {
+    const newProject = new Project('personal', 'my personal stuff')
+    console.log(newProject)
+})
+
+
+
+
---
+++ src/project.js
@@ -0,0 +1,10 @@
+class Project {
+    constructor(name, description, taskArr = []) {
+        this.name = name
+        this.description = description
+        this.taskArr = taskArr
+    }
+}
+
+
+export { Project }
